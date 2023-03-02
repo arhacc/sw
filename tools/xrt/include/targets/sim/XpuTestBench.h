b5d79028ff96c98d7528447f84f3d9065cca01a7
alex13dumi <alex13.dumi@gmail.com>
2023-03-02 13:57:41 +0200

    Added methods: readAxiSignals() -> reads values from AXI ports; int extractBits(int number, int k, int p) -> extracts k bits from position p from a number "number" Axi_Lite_Write/Read work fine, all signals are the same like SV Testbench

--- tools/xrt/include/targets/sim/XpuTestBench.h
+++ tools/xrt/include/targets/sim/XpuTestBench.h
@@ -26,11 +26,11 @@ typedef struct{
     bool is_input;
 } port_parameters;
 
-//-------------------------------------------------------------------------------------
-class XpuTestBench {
+class XpuTestBench
+{
 public:
     XpuTestBench(const std::string& design_libname, const std::string& simkernel_libname,
-         float clock_period_ns, const std::string& clock_name, const std::string& reset_name);
+                 float clock_period_ns, const std::string& clock_name, const std::string& reset_name);
     ~XpuTestBench();
 
     unsigned int num_ports();
@@ -39,38 +39,48 @@ public:
     void write(const std::string &port_name, unsigned int val);
     unsigned int read(const std::string &port_name);
     void restart();
-    void wait_clock_cycles(unsigned int nr_clock_cycles);
     void do_reset(unsigned int nr_active_clock_cycles);
+    void write_addr(uint32_t data);
+    void write_data(uint32_t data);
+    void wait_high_clock();
+    void wait_low_clock();
+    void wait_clock();
+    bool axi_bvalid_ready();
+    void wait_clock_cycles(unsigned int nr_clock_cycles);
+    XSI_INT64 get_time();
+    void generate_clock(int clock_half_period);
+
     /*Testbench init*/
     void tb_init();
     void AXI_init();
-    void write_addr(uint32_t data);
-    void write_data(uint32_t data);
-    void wait_high();
-    void wait_low();
-    void wait();
-
-    void AXI_Lite_write(unsigned int waddr, unsigned int wdata);
-    void AXI_Lite_read(unsigned int raddr);
+    void readAxiSignals();
+    void AXI_Lite_write(uint32_t waddr, uint32_t wdata);
+    void AXI_Lite_read(uint32_t raddr);
     void AXI_Stream_SendData(unsigned int data);
     void AXI_Stream_SendStop();
     void AXI_Stream_receive_data();
     void AXI_Stream_stop_receive_data();
 
-
-    const s_xsi_vlog_logicval one_val  = {0X00000001, 0X00000000};
-    const s_xsi_vlog_logicval zero_val = {0X00000000, 0X00000000};
     XSI_INT32 kernel_precision;
     XSI_INT32 time_ticks;
 
-private:
-    Xsi::Loader xsi;
-    std::map<std::string, port_parameters> port_map;
+    std::fstream axi_stream_out_file;
+    std::fstream axi_stream_in_file;
+#ifdef SIMULATION_GOLDEN_MODEL_CHECKS
+    std::fstream golden_model_out_file;
+#endif
+    std::fstream axi_lite_program_file_combined;
+    std::fstream axi_lite_program_file;
+    std::fstream axi_lite_function_arguments_file;
+
     unsigned int clock_half_period;
+    Xsi::Loader xsi;
+
+private:
+    std::map<std::string, port_parameters> port_map;
     unsigned int cycle_count;
     std::string clock;
     std::string reset;
-    std::fstream file;
     int test_done_ok;
 };
 
