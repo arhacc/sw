0c6ff8220f88aa059504a9970a0a29d267421210
Serbu Vlad <serbuvladgabriel@gmail.com>
2023-08-07 13:54:21 +0300

    Formatted Target.h file

--- tools/xrt/include/targets/common/Target.h
+++ tools/xrt/include/targets/common/Target.h
@@ -24,31 +24,6 @@ class Target {
 
     virtual void runRuntime(uint32_t _address, uint32_t _argc, uint32_t* _args) = 0;
 
-    virtual void
-    runDebug(uint32_t _address, uint32_t* _args, uint32_t _breakpointAddress) = 0;
-
-    virtual uint32_t readRegister(uint32_t _address) = 0;
-
-    virtual void writeRegister(uint32_t _address, uint32_t _register) = 0;
-
-    virtual void writeCode(uint32_t _address, uint32_t* _code, uint32_t _length);
-
-    virtual void readControllerData(
-        uint32_t _address,
-        uint32_t* _data,
-        uint32_t _lineStart,
-        uint32_t _lineStop,
-        uint32_t _columnStart,
-        uint32_t _columnStop) = 0;
-
-    virtual void writeControllerData(
-        uint32_t _address,
-        uint32_t* _data,
-        uint32_t _lineStart,
-        uint32_t _lineStop,
-        uint32_t _columnStart,
-        uint32_t _columnStop) = 0;
-
     virtual void readMatrixArray(
         uint32_t _accMemStart,
         uint32_t* _ramMatrix,
@@ -69,6 +44,33 @@ class Target {
         uint32_t _ramStartColumn,
         uint32_t _numLines,
         uint32_t _numColumns) = 0;
+
+    virtual uint32_t readRegister(uint32_t _address) = 0;
+
+    virtual void writeRegister(uint32_t _address, uint32_t _register) = 0;
+
+    virtual void writeCode(uint32_t _address, uint32_t* _code, uint32_t _length);
+
+    // unimplemented yet
+
+    virtual void
+    runDebug(uint32_t _address, uint32_t* _args, uint32_t _breakpointAddress) = 0;
+
+    virtual void readControllerData(
+        uint32_t _address,
+        uint32_t* _data,
+        uint32_t _lineStart,
+        uint32_t _lineStop,
+        uint32_t _columnStart,
+        uint32_t _columnStop) = 0;
+
+    virtual void writeControllerData(
+        uint32_t _address,
+        uint32_t* _data,
+        uint32_t _lineStart,
+        uint32_t _lineStop,
+        uint32_t _columnStart,
+        uint32_t _columnStop) = 0;
 
     virtual void dump(const std::string& _address) = 0;
 };
