3370e3b509bd16c9f94b6dcd7797704c66bb79db
Serbu Vlad-Gabriel <serbuvladgabriel@gmail.com>
2023-10-15 17:23:11 +0300

    Remove obsolete scripts

--- install.bat
+++
@@ -1,4 +0,0 @@
-
-mkdir %HOME%\.xpu
-xcopy /E /I %HOME%\build_project\project\xpu\sw\tools\installer\dirs\.xpu %HOME%\.xpu
-copy %HOME%\build_project\project\xpu\sw\tools\installer\dirs\Desktop\XPU.lnk %HOME%\Desktop\
