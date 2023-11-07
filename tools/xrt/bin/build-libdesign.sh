d81f3483095e6f0ae93970b65b337fc62dc034b0
marius <marius.stoian@upb.ro>
2023-11-08 05:13:04 +0530

    add simulator_axi_vlog.prj

--- tools/xrt/bin/build-libdesign.sh
+++ tools/xrt/bin/build-libdesign.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #source "./setenv.sh"
 
-xelab -prj $ARHACC_PATH/hw/low_level/xpu/xpu.sim/sim_1/behav/xsim/simulator_axi_vlog.prj \
+xelab -prj $ARHACC_PATH/sw/tools/xrt/etc/simulator_axi_vlog.prj \
 xil_defaultlib.xpu xil_defaultlib.glbl -dll -debug all --relax --mt 8 -s simulator_axi   \
 -i $ARHACC_PATH/hw/low_level/xpu/xpu.srcs/sim_1/imports/new/                                  \
 -i $ARHACC_PATH/hw/low_level/xpu/xpu.srcs/sources_1/imports/new                               \
