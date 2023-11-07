a2a641fd8887f1994982b0138c3ff2e23d090434
marius <marius.stoian@upb.ro>
2023-11-08 01:31:01 +0530

    Update build-libdesign.sh

--- tools/xrt/bin/build-libdesign.sh
+++ tools/xrt/bin/build-libdesign.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 #source "./setenv.sh"
 
-xelab -prj $XPU_HW_PATH/low_level/xpu/xpu.sim/sim_1/behav/xsim/simulator_axi_vlog.prj \
+xelab -prj $ARHACC_PATH/hw/low_level/xpu/xpu.sim/sim_1/behav/xsim/simulator_axi_vlog.prj \
 xil_defaultlib.xpu xil_defaultlib.glbl -dll -debug all --relax --mt 8 -s simulator_axi   \
--i $XPU_HW_PATH/low_level/xpu/xpu.srcs/sim_1/imports/new/                                  \
--i $XPU_HW_PATH/low_level/xpu/xpu.srcs/sources_1/imports/new                               \
--i $XPU_HW_PATH/low_level/xpu/xpu.srcs/sources_1/new                                       \
+-i $ARHACC_PATH/hw/low_level/xpu/xpu.srcs/sim_1/imports/new/                                  \
+-i $ARHACC_PATH/hw/low_level/xpu/xpu.srcs/sources_1/imports/new                               \
+-i $ARHACC_PATH/hw/low_level/xpu/xpu.srcs/sources_1/new                                       \
 -L xil_defaultlib -L uvm -L unisims_ver                                                      \
 -L unimacro_ver -L secureip -log elaborate.log -stat --nolog
 
