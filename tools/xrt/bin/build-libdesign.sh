8a8552892c3d71ebde43087aa859c2e79cb6663a
marius <marius.stoian@upb.ro>
2023-11-08 06:30:41 +0530

    Update build-libdesign.sh

--- tools/xrt/bin/build-libdesign.sh
+++ tools/xrt/bin/build-libdesign.sh
@@ -2,7 +2,7 @@
 #source "./setenv.sh"
 
 xelab -prj $ARHACC_PATH/sw/tools/xrt/etc/simulator_axi_vlog.prj \
-xil_defaultlib.xpu xil_defaultlib.glbl -dll -debug all --relax --mt 8 -s simulator_axi   \
+xil_defaultlib.xpu xil_defaultlib.glbl -dll -debug all --relax --mt 8 -s simulator_axi \
 -i $ARHACC_PATH/hw/low_level/xpu/xpu.srcs/sim_1/imports/new/                                  \
 -i $ARHACC_PATH/hw/low_level/xpu/xpu.srcs/sources_1/imports/new                               \
 -i $ARHACC_PATH/hw/low_level/xpu/xpu.srcs/sources_1/new                                       \
