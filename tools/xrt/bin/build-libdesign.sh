d5b734048cb79811349c34a401349e22b6323825
Serbu Vlad-Gabriel <serbuvladgabriel@gmail.com>
2023-11-30 18:58:24 +0200

    Change build-libdesign script to use prj file in HW repo

--- tools/xrt/bin/build-libdesign.sh
+++ tools/xrt/bin/build-libdesign.sh
@@ -6,14 +6,14 @@ then
     rm -r $XPU_HOME/lib/xsim.dir
 fi
 
-xelab -prj $ARHACC_PATH/sw/tools/xrt/etc/simulator_axi_vlog.prj \
+xelab -prj "$XPU_HW_PATH/low_level/xpu/simulator_include/xpu_simulator.prj" \
 xil_defaultlib.xpu xil_defaultlib.glbl -dll -debug all --relax --mt 8 -s simulator_axi \
--i $ARHACC_PATH/hw/low_level/xpu/xpu.srcs/sim_1/imports/new                                   \
--i $ARHACC_PATH/hw/low_level/xpu/xpu.srcs/sources_1/imports/new                               \
--i $ARHACC_PATH/hw/low_level/xpu/xpu.srcs/sources_1/new                                       \
+-i "$XPU_HW_PATH/low_level/xpu/xpu.srcs/sim_1/imports/new"                                   \
+-i "$XPU_HW_PATH/low_level/xpu/xpu.srcs/sources_1/imports/new"                               \
+-i "$XPU_HW_PATH/low_level/xpu/xpu.srcs/sources_1/new"                                       \
 -L xil_defaultlib -L uvm -L unisims_ver                                                      \
 -L unimacro_ver -L secureip -log elaborate.log -stat --nolog
 
-cp -rf xsim.dir $XPU_HOME/lib
+cp -rf xsim.dir "$XPU_HOME/lib"
 rm -r xsim.dir
 rm xelab.pb
