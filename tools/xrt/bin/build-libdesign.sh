4366cbcc9cc26df17a57a5fba397a9ef813e92d2
marius <marius.stoian@upb.ro>
2023-12-15 19:55:16 +0530

    Update build-libdesign.sh

--- tools/xrt/bin/build-libdesign.sh
+++ tools/xrt/bin/build-libdesign.sh
@@ -12,7 +12,7 @@ xil_defaultlib.simulator_axi_xrt xil_defaultlib.glbl -dll -debug all --relax --m
 -i "$XPU_HW_PATH/low_level/xpu/xpu.srcs/sources_1/imports/new"                               \
 -i "$XPU_HW_PATH/low_level/xpu/xpu.srcs/sources_1/new"                                       \
 -L xil_defaultlib -L uvm -L unisims_ver -L all                                                      \
--L unimacro_ver -L secureip -log elaborate.log -stat 
+-L unimacro_ver -L secureip -log elaborate.log -stats -v 2 
 
 cp -rf xsim.dir "$XPU_HOME/lib"
 rm -r xsim.dir
