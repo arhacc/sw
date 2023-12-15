f960fff44f9944dbfb88d78e92008a03c961e56e
marius <marius.stoian@upb.ro>
2023-12-15 19:58:10 +0530

    Update build-libdesign.sh

--- tools/xrt/bin/build-libdesign.sh
+++ tools/xrt/bin/build-libdesign.sh
@@ -7,12 +7,12 @@ then
 fi
 
 xelab -prj "$XPU_HW_PATH/low_level/xpu/simulator_include/xpu_simulator.prj" \
-xil_defaultlib.simulator_axi_xrt xil_defaultlib.glbl -dll -debug all --relax --mt 8 -s simulator_axi \
+xil_defaultlib.xpu xil_defaultlib.glbl -dll -debug all --relax --mt 8 -s simulator_axi \
 -i "$XPU_HW_PATH/low_level/xpu/xpu.srcs/sim_1/imports/new"                                   \
 -i "$XPU_HW_PATH/low_level/xpu/xpu.srcs/sources_1/imports/new"                               \
 -i "$XPU_HW_PATH/low_level/xpu/xpu.srcs/sources_1/new"                                       \
 -L xil_defaultlib -L uvm -L unisims_ver -L all                                                      \
--L unimacro_ver -L secureip -log elaborate.log -stats -v 2 
+-L unimacro_ver -L secureip -log elaborate.log -stats -v 0 
 
 cp -rf xsim.dir "$XPU_HOME/lib"
 rm -r xsim.dir
