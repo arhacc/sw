9184a5f88a108fc0c24a5ae8ac117b1d6dcf78af
Serbu Vlad-Gabriel <serbuvladgabriel@gmail.com>
2024-01-09 00:15:08 +0200

    Fix memset size

--- tools/xrt/src/common/types/Matrix.cpp
+++ tools/xrt/src/common/types/Matrix.cpp
@@ -16,7 +16,7 @@ Matrix::Matrix(size_t _numRows, size_t _numColumns)
     : numRows_(_numRows), numColumns_(_numColumns) {
     data = new uint32_t[_numRows * _numColumns];
 
-    std::memset(data, 0, _numRows * _numColumns);
+    std::memset(data, 0, _numRows * _numColumns * sizeof(uint32_t));
 }
 
 Matrix::~Matrix() {
@@ -39,7 +39,7 @@ const uint32_t& Matrix::at(size_t i, size_t j) const {
 
 void Matrix::resize(size_t _newNumRows, size_t _newNumColumns) {
     uint32_t* newData = new uint32_t[_newNumRows * _newNumColumns];
-    std::memset(newData, 0, _newNumRows * _newNumColumns);
+    std::memset(newData, 0, _newNumRows * _newNumColumns * sizeof(uint32_t));
 
     for (size_t i = 0; i < std::min(_newNumRows, numRows_); i++) {
         for (size_t j = 0; j < std::min(_newNumRows, numRows_); j++) {
