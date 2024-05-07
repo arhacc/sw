6f27646f1ef4d5a435bcb1c5134f78a84b069f0f
Vlad-Gabriel Serbu <serbuvladgabriel@gmail.com>
2024-05-07 18:15:50 +0300

    Fix LD_LIBRARY_PATH in debug script

--- tools/xrt/bin/debug.sh
+++ tools/xrt/bin/debug.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 
+export LD_LIBRARY_PATH="${LD_LIBRARY_PATH}:$(patchelf --print-rpath ./build/bin/xrt | sed -E 's/:\$ORIGIN[^:]*:?//g')"
+
 echo gdb "./build/bin/xrt" "$@"
 gdb --args ./build/bin/xrt "$@"
