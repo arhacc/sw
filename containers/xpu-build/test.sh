848fc173c2e385f05e267b52771e1cfaf1ab8b76
Vlad-Gabriel Șerbu <serbuvladgabriel@gmail.com>
2024-10-21 13:01:20 +0300

    container upgrades

---
+++ containers/xpu-build/test.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+set -ex
+
+export LD_LIBRARY_PATH="${XILINX_VIVADO}/lib/lnx64.o"
+source "${XILINX_VIVADO}/settings64.sh"
+
+export XPU_HOME=/root/.xpu
+
+cd /root
+cp "${ZIP_FILE}" .
+unzip "${ZIP_FILE}"
+
+source /root/.xpu/bin/test/all_tests.sh
